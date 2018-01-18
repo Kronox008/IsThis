586c98838589b4c3b2d882c1dc56250d2f46216a
Kronox008 <34090940+Kronox008@users.noreply.github.com>
2018-01-18 21:31:52 +0200

    updated Lord and friends decks

--- IsThis/IsThis/IsThis/Decks/FriendsDeck.cs
+++ IsThis/IsThis/IsThis/Decks/FriendsDeck.cs
@@ -12,21 +12,57 @@ namespace IsThis.Decks
         {
             if(Global.LanguageSelectedID == 0)
             { 
-            string[ , ] allQuestions_in_orderEN = { { "Gunther", "Gunther.jpg" }, { "Jack Geller", "jack_geller.jpg"}, {"Ursula Buffay", "Ursula_Buffay.jpg"},
-            { "Marcel","marcel.jpg" }, {"Paolo", "Paolo.jpg"}, {"David", "David.jpg"}, {"Julie","Julie.jpg" }, {"Estelle Leonard","Estelle_Leonard.jpg" },
-            {"Mike Hannigan","Mike_Hannigan.jpg" },  {"Janine LaCroix", "Janine_LaCroix.jpg" }, { "Judy Geller", "Judy_Geller.jpg" },  
-            { "Barry Farber", "Barry_Farber.jpg" }, { "Carol Willick", "Carol_Willick.jpg" },   { "Susan Bunch", "Susan_Bunch.jpg" },  
-            { "Janice Litman Goralnik", "Janice_Litman_Goralnik.jpg" }, { "Mr. Heckles", "Mr_Heckles.jpg" },    { "Nora Tyler Bing", "Nora_Tyler_Bing.jpg" },
-            { "Steve", "Steve.jpg" },   { "Mr. Treeger", "Mr_Treeger.jpg" },    { "Dr. Richard Burke", "Dr_Richard_Burke.jpg" },    
-            { "Frank Buffay Jr.", "Frank_Buffay_Jr.jpg" },  { "Leonard Green", "Leonard_Green.jpg" },   { "Sandra Green", "Sandra_Green.jpg" }, 
-            { "Eddie Manoick", "Eddie_Manoick.jpg" },   { "Mark Robinson", "Mark_Robinson.jpg" },   { "Sophie", "Sophie.jpg" }, { "Phoebe Abbott", "Phoebe_Abbott.jpg" },
-            { "Pete Becker", "Pete_Becker.jpg" },   { "Alice Knight Buffay", "Alice_Knight_Buffay.jpg" },   { "Kathy", "Kathy.jpg" },  
-            { "Emily Waltham", "Emily_Waltham.jpg" },   { "Joshua Burgin", "Joshua_Burgin.jpg" },   { "Mr. Zelner", "Mr_Zelner.jpg" },  { "Gary", "Gary.jpg" },
-            { "Elizabeth Stevens", "Elizabeth_Stevens.jpg" },   { "Jill Green", "Jill_Green.jpg" }, { "Tag Jones", "Tag_Jones.jpg" },   { "Mona", "Mona.jpg" },
-            { "Dr. Long", "Dr_Long.jpg" },  { "Amy Green", "Amy_Green.jpg" },   { "Charlie Wheeler", "Charlie_Wheeler.jpg" },   { "Erica", "Erica.jpg" },
-            { "Smelly Cat", "Smelly_Cat.jpg" }, { "Unagi", "Unagi.jpg" },   { "Red Ross", "Red_Ross.jpg" }, { "Yemen", "Yemen.jpg" },  
-            { "Armadillo", "Armadillo.jpg" },   { "In Prison!", "In_Prison.jpg" },  { "Wah! Pah!", "Wah_Pah.jpg" },
-            { "Central Perk", "Central_Perk.jpg" }
+            string[ , ] allQuestions_in_orderEN = {
+  { "Alice Knight Buffay", "https://drive.google.com/uc?id=160vcQbE4zBEB9itAqPaCkXeXkPzUt2gG" }
+, { "Amy Green", "https://drive.google.com/uc?id=1rjvYDXjsoXyD84I75f_njmM2AMO84dZ6" }
+, { "Armadillo", "https://drive.google.com/uc?id=1NoN34Gs7tBAHu5EBYfuX-T6xt4a5pvch" }
+, { "Barry Farber", "https://drive.google.com/uc?id=1MJDaHTqWWVE8iJFBeV5Zx0usSmSVCDiH" }
+, { "Carol Willick", "https://drive.google.com/uc?id=1FbMBu2QC44HnzOa8Z8iMgeiVxephu_ba" }
+, { "Central Perk", "https://drive.google.com/uc?id=1UKmbIbrEZXkKeBHf-bwFkOF7uWDToo6j" }
+, { "Charlie Wheeler", "https://drive.google.com/uc?id=1FbMBu2QC44HnzOa8Z8iMgeiVxephu_ba" }
+, { "David", "https://drive.google.com/uc?id=1pctac4n93rGyzc6DOnmNi6nU4KQ-2PwX" }
+, { "Dr. Richard Burke", "https://drive.google.com/uc?id=18NeKmm0tTGbcDTqy_4F14p7cD4OR4Ac2" }
+, { "Eddie Manoick", "https://drive.google.com/uc?id=1DKBrPtl53LOaVSJW6L-E6DcECM1U3U9-" }
+, { "Elizabeth Stevens", "https://drive.google.com/uc?id=10KvUapCdpjqPL-FZWZ5P2CdBSrgt8ZAP" }
+, { "Emily Waltham", "https://drive.google.com/uc?id=1EjlfYszMl63oWrm1Zizi5-SSIg22SENr" }
+, { "Erica", "https://drive.google.com/uc?id=1YgYz0dJGB2hSVGsINMBeljhHhdre3CWf" }
+, { "Estelle Leonard", "https://drive.google.com/uc?id=1i6ij2jfVBAp9O4sOue3zFSqlzSG1_vke" }
+, { "Frank Buffay Jr.", "https://drive.google.com/uc?id=1ELqZXL5XVK4B5VCGc6Cx4MLzb6krSxZP" }
+, { "Gary", "https://drive.google.com/uc?id=1mq2gbb_gR3Cmc16Q2ZDqZrjWpeb5Bb6w" }
+, { "Gunther", "https://drive.google.com/uc?id=1wQX-iUml2iO_LgDUXEiY0rtc4WRuyhpL" }
+, { "Yemen", "https://drive.google.com/uc?id=12tlIEIesfdnn1QQC8f4uIks14S5v5_pJ" }
+, { "In Prison!", "https://drive.google.com/uc?id=1OfamY4Lj7qGRobW2uZBA-vIE4efGtcaE" }
+, { "Jack Geller", "https://drive.google.com/uc?id=1pnWKw6dUSV5zX5rUG04fIxW90jzna8BN" }
+, { "Janice Litman Goralnik", "https://drive.google.com/uc?id=1UmZ6qdU7aIJI6J8anf9ax4OZhxIAu-if" }
+, { "Janine LaCroix", "https://drive.google.com/uc?id=1dSvzu4y6mDFeJJJ2By1MYyzIyvYkL8CQ" }
+, { "Jill Green", "https://drive.google.com/uc?id=1X_BlZxaoLyd91rve8zuxVEAt_i_cOB3W" }
+, { "Joshua Burgin", "https://drive.google.com/uc?id=1N4vrseYnjUafPQTpSYrQSIueCg14pZIJ" }
+, { "Judy Geller", "https://drive.google.com/uc?id=14r6s-agH3fWxqVLrGDft_PcZ4dP8ii39" }
+, { "Julie", "https://drive.google.com/uc?id=1hGQ_U1WoC6d3TJ89K723psfFDdLUcX5l" }
+, { "Kathy", "https://drive.google.com/uc?id=14JwN_F1TicAfK2n8csGJ2BMylXnYXzal" }
+, { "Leonard Green", "https://drive.google.com/uc?id=1Qar6qCrxSCfp-XsX90vTyMmo8y8UHtMI" }
+, { "Marcel", "https://drive.google.com/uc?id=1N3UnPUUNwq4_htyMWBsRUCxbN1u_ZnFC" }
+, { "Mark Robinson", "https://drive.google.com/uc?id=1wEpHxOWWffDrmPFCRJWNb3YRH5w8ZmPh" }
+, { "Melon drink", "https://drive.google.com/uc?id=1dx5pMecUKK9QF9M18_z8HDrJX6oJz1Q-" }
+, { "Mike Hannigan", "https://drive.google.com/uc?id=1k1zi1oqO2-JOFSjYdahI8UhWB0NEFORv" }
+, { "Mona", "https://drive.google.com/uc?id=16lf9bnEkpE4qSksEGLLXHkRqSX21OjZI" }
+, { "Mr. Heckles", "https://drive.google.com/uc?id=1MQBUk6ggP-_kVhKi8rEZBtpbDDTFvIQb" }
+, { "Mr. Treeger", "https://drive.google.com/uc?id=1VtcFCyYAK8_ycR6jim9JpNBdlmDyW5jC" }
+, { "Mr. Zelner", "https://drive.google.com/uc?id=1VFTw1aTKaq3Nzj28YTBdV1BQuvnaSMPr" }
+, { "Nora Tyler Bing", "https://drive.google.com/uc?id=1UbBoBsdiouUFuhJZJXYBFk852ZZazYr9" }
+, { "Paolo", "https://drive.google.com/uc?id=1F5biFGqNVV1fBsFxG9XUDkpuL4meqy4c" }
+, { "Pete Becker", "https://drive.google.com/uc?id=18JaCONQTbrehVCLeMxXgrAzBbSaovlTH" }
+, { "Phoebe Abbott", "https://drive.google.com/uc?id=1kjYlHOO5ynQwoa0y3CvcWApWxu4-0Fym" }
+, { "Red Ross", "https://drive.google.com/uc?id=1jBygh914e_a-cPjiLowZ6XE_zcxZgO3w" }
+, { "Sandra Green", "https://drive.google.com/uc?id=1qOIHkxCN7WAfSIzrbmf5ShxHEZucDaq5" }
+, { "Smelly Cat", "https://drive.google.com/uc?id=1tdDNYfh4O3naFTdW3Py7ZKuuYJke5-tS" }
+, { "Sophie", "https://drive.google.com/uc?id=12v4urLK6ls4Trc8qVU5_Ff67rxijUWI4" }
+, { "Steve", "https://drive.google.com/uc?id=1wOtx72vOdTZF6ie0V-CdtgZcbvfqQvMM" }
+, { "Susan Bunch", "https://drive.google.com/uc?id=1f289dPi68DaJtmrhPkl5ZvdQb3CRQPZA" }
+, { "Tag Jones", "https://drive.google.com/uc?id=1tu5FEosqJ0c48ZpG9SON_g85dIW-XmeO" }
+, { "Unagi", "https://drive.google.com/uc?id=1SJY38k4WtYBZGbqgwVF1JTi0_nMGlN6l" }
+, { "Ursula Buffay", "https://drive.google.com/uc?id=1W3Ssu003HxAdc_MAo-JaA1Kqt6rDCd6N" }
+, { "Wah! Pah!", "https://drive.google.com/uc?id=1qE3Df08S58qiZNKVY71F58MnJOQooJSD" }
 
 
                 };
@@ -51,20 +87,56 @@ namespace IsThis.Decks
             {
                 string[,] allQuestions_in_orderLT = {
 
-                    { "Džiudi Geler", "Judy_Geller.jpg" } , { "Baris Faberis", "Barry_Farber.jpg" }   , { "Kerol Vilik", "Carol_Willick.jpg" }    
-                    , { "Siuzan Bunč", "Susan_Bunch.jpg" }  , { "Dženis Litman Goralnik", "Janice_Litman_Goralnik.jpg" }    , { "Ponas Heklsas", "Mr_Heckles.jpg" }
-                    , { "Nora Tailer Bing", "Nora_Tyler_Bing.jpg" } , { "Styvas", "Steve.jpg" } , { "Ponas Trigeris", "Mr_Treeger.jpg" } ,
-                    { "Ričardas Berkas", "Dr_Richard_Burke.jpg" } , { "Frenkas Biufei jaunesnynis", "Frank_Buffay_Jr.jpg" }    , { "Leonardas Green", "Leonard_Green.jpg" } 
-                    , { "Sandra Green", "Sandra_Green.jpg" }    , { "Edis Manoickas", "Eddie_Manoick.jpg" } , { "Markas Robinsonas", "Mark_Robinson.jpg" }  , { "Sofi", "Sophie.jpg" } 
-                    , { "Fibi Abot", "Phoebe_Abbott.jpg" }  , { "Pitas Bekeris", "Pete_Becker.jpg" }    , { "Elis Nait Bufei", "Alice_Knight_Buffay.jpg" }  , { "Keiti", "Kathy.jpg" } 
-                    , { "Emili Voltham", "Emily_Waltham.jpg" }  , { "Džošuo Burginas", "Joshua_Burgin.jpg" }    , { "Ponas Zelneris", "Mr_Zelner.jpg" } , { "Garis", "Gary.jpg" }  
-                    , { "Elizabet Stevens", "Elizabeth_Stevens.jpg" }   , { "Jil Green", "Jill_Green.jpg" } , { "Tagas Jaunsas", "Tag_Jones.jpg" }  , { "Mona", "Mona.jpg" }  
-                    , { "Daktarė Long", "Dr_Long.jpg" } , { "Eimi Green", "Amy_Green.jpg" } , { "Čarli Vyner", "Charlie_Wheeler.jpg" }  , { "Erika", "Erica.jpg" }
-                    , { "Giunteris", "Gunther.jpg" }    , { "Jekas Geleris", "Jack_Geller.jpg" }    , { "Uršula Bufei", "Ursula_Buffay.jpg" }   , { "Marselis", "Marcel.jpg" }  
-                    , { "Paulo", "Paolo.jpg" }  , { "Deividas", "David.jpg" }   , { "Džiuli", "Julie.jpg" } , { "Estelė Leonard", "Estelle_Leonard.jpg" }   
-                    , { "Maikas Haniganas", "Mike_Hannigan.jpg" }   , { "Džanin Lacroiks", "Janine_LaCroix.jpg" }   , { "Dvokianti Katė", "Smelly_Cat.jpg" }    
-                    , { "Unagi", "Unagi.jpg" }  , { "Raudonasis Rosas", "Red_Ross.jpg" }    , { "Jemenas", "Yemen.jpg" }    , { "Šarvuotis", "Armadillo.jpg" } 
-                    , { "Kalėjime!", "In_Prison.jpg" }  , { "Va! Pach!", "Wah_Pah.jpg" }    , { "Centrinis Perkas", "Central_Perk.jpg" }
+  { "Elis Nait Bufei", "https://drive.google.com/uc?id=160vcQbE4zBEB9itAqPaCkXeXkPzUt2gG" }
+, { "Eimi Gryn", "https://drive.google.com/uc?id=1rjvYDXjsoXyD84I75f_njmM2AMO84dZ6" }
+, { "Šarvuotis", "https://drive.google.com/uc?id=1NoN34Gs7tBAHu5EBYfuX-T6xt4a5pvch" }
+, { "Baris Faberis", "https://drive.google.com/uc?id=1MJDaHTqWWVE8iJFBeV5Zx0usSmSVCDiH" }
+, { "Kerol Vilik", "https://drive.google.com/uc?id=1FbMBu2QC44HnzOa8Z8iMgeiVxephu_ba" }
+, { "Centrinis Perkas", "https://drive.google.com/uc?id=1UKmbIbrEZXkKeBHf-bwFkOF7uWDToo6j" }
+, { "Čarli Vyner", "https://drive.google.com/uc?id=1FbMBu2QC44HnzOa8Z8iMgeiVxephu_ba" }
+, { "Deividas", "https://drive.google.com/uc?id=1pctac4n93rGyzc6DOnmNi6nU4KQ-2PwX" }
+, { "Ričardas Berkas", "https://drive.google.com/uc?id=18NeKmm0tTGbcDTqy_4F14p7cD4OR4Ac2" }
+, { "Edis Manoickas", "https://drive.google.com/uc?id=1DKBrPtl53LOaVSJW6L-E6DcECM1U3U9-" }
+, { "Elizabet Styvens", "https://drive.google.com/uc?id=10KvUapCdpjqPL-FZWZ5P2CdBSrgt8ZAP" }
+, { "Emili Voltham", "https://drive.google.com/uc?id=1EjlfYszMl63oWrm1Zizi5-SSIg22SENr" }
+, { "Erika", "https://drive.google.com/uc?id=1YgYz0dJGB2hSVGsINMBeljhHhdre3CWf" }
+, { "Estelė Leonard", "https://drive.google.com/uc?id=1i6ij2jfVBAp9O4sOue3zFSqlzSG1_vke" }
+, { "Frenkas Biufei jaunesnynis", "https://drive.google.com/uc?id=1ELqZXL5XVK4B5VCGc6Cx4MLzb6krSxZP" }
+, { "Garis", "https://drive.google.com/uc?id=1mq2gbb_gR3Cmc16Q2ZDqZrjWpeb5Bb6w" }
+, { "Giunteris", "https://drive.google.com/uc?id=1wQX-iUml2iO_LgDUXEiY0rtc4WRuyhpL" }
+, { "Jemenas", "https://drive.google.com/uc?id=12tlIEIesfdnn1QQC8f4uIks14S5v5_pJ" }
+, { "Kalėjime!", "https://drive.google.com/uc?id=1OfamY4Lj7qGRobW2uZBA-vIE4efGtcaE" }
+, { "Jekas Geleris", "https://drive.google.com/uc?id=1pnWKw6dUSV5zX5rUG04fIxW90jzna8BN" }
+, { "Dženis Litman Goralnik", "https://drive.google.com/uc?id=1UmZ6qdU7aIJI6J8anf9ax4OZhxIAu-if" }
+, { "Džanin Lacroiks", "https://drive.google.com/uc?id=1dSvzu4y6mDFeJJJ2By1MYyzIyvYkL8CQ" }
+, { "Jil Gryn", "https://drive.google.com/uc?id=1X_BlZxaoLyd91rve8zuxVEAt_i_cOB3W" }
+, { "Džošuo Burginas", "https://drive.google.com/uc?id=1N4vrseYnjUafPQTpSYrQSIueCg14pZIJ" }
+, { "Džiudi Geler", "https://drive.google.com/uc?id=14r6s-agH3fWxqVLrGDft_PcZ4dP8ii39" }
+, { "Džiuli", "https://drive.google.com/uc?id=1hGQ_U1WoC6d3TJ89K723psfFDdLUcX5l" }
+, { "Keiti", "https://drive.google.com/uc?id=14JwN_F1TicAfK2n8csGJ2BMylXnYXzal" }
+, { "Leonardas Grynas", "https://drive.google.com/uc?id=1Qar6qCrxSCfp-XsX90vTyMmo8y8UHtMI" }
+, { "Marselis", "https://drive.google.com/uc?id=1N3UnPUUNwq4_htyMWBsRUCxbN1u_ZnFC" }
+, { "Markas Robinsonas", "https://drive.google.com/uc?id=1wEpHxOWWffDrmPFCRJWNb3YRH5w8ZmPh" }
+, { "Melionų gėrimas", "https://drive.google.com/uc?id=1dx5pMecUKK9QF9M18_z8HDrJX6oJz1Q-" }
+, { "Maikas Haniganas", "https://drive.google.com/uc?id=1k1zi1oqO2-JOFSjYdahI8UhWB0NEFORv" }
+, { "Mona", "https://drive.google.com/uc?id=16lf9bnEkpE4qSksEGLLXHkRqSX21OjZI" }
+, { "Ponas Heklsas", "https://drive.google.com/uc?id=1MQBUk6ggP-_kVhKi8rEZBtpbDDTFvIQb" }
+, { "Ponas Trigeris", "https://drive.google.com/uc?id=1VtcFCyYAK8_ycR6jim9JpNBdlmDyW5jC" }
+, { "Ponas Zelneris", "https://drive.google.com/uc?id=1VFTw1aTKaq3Nzj28YTBdV1BQuvnaSMPr" }
+, { "Nora Tailer Bing", "https://drive.google.com/uc?id=1UbBoBsdiouUFuhJZJXYBFk852ZZazYr9" }
+, { "Paulo", "https://drive.google.com/uc?id=1F5biFGqNVV1fBsFxG9XUDkpuL4meqy4c" }
+, { "Pitas Bekeris", "https://drive.google.com/uc?id=18JaCONQTbrehVCLeMxXgrAzBbSaovlTH" }
+, { "Fibi Abot", "https://drive.google.com/uc?id=1kjYlHOO5ynQwoa0y3CvcWApWxu4-0Fym" }
+, { "Raudonasis Rosas", "https://drive.google.com/uc?id=1jBygh914e_a-cPjiLowZ6XE_zcxZgO3w" }
+, { "Sandra Gryn", "https://drive.google.com/uc?id=1qOIHkxCN7WAfSIzrbmf5ShxHEZucDaq5" }
+, { "Dvokianti Katė", "https://drive.google.com/uc?id=1tdDNYfh4O3naFTdW3Py7ZKuuYJke5-tS" }
+, { "Sofi", "https://drive.google.com/uc?id=12v4urLK6ls4Trc8qVU5_Ff67rxijUWI4" }
+, { "Styvas", "https://drive.google.com/uc?id=1wOtx72vOdTZF6ie0V-CdtgZcbvfqQvMM" }
+, { "Siuzan Bunč", "https://drive.google.com/uc?id=1f289dPi68DaJtmrhPkl5ZvdQb3CRQPZA" }
+, { "Tagas Jaunsas", "https://drive.google.com/uc?id=1tu5FEosqJ0c48ZpG9SON_g85dIW-XmeO" }
+, { "Unagi", "https://drive.google.com/uc?id=1SJY38k4WtYBZGbqgwVF1JTi0_nMGlN6l" }
+, { "Uršula Bufei", "https://drive.google.com/uc?id=1W3Ssu003HxAdc_MAo-JaA1Kqt6rDCd6N" }
+, { "Va! Pach!", "https://drive.google.com/uc?id=1qE3Df08S58qiZNKVY71F58MnJOQooJSD" }
 
                 };
 
--- IsThis/IsThis/IsThis/Decks/LordDeck.cs
+++ IsThis/IsThis/IsThis/Decks/LordDeck.cs
@@ -13,13 +13,58 @@ namespace IsThis.Decks
         {
             if (Global.LanguageSelectedID == 0)
             {
-                string[,] allQuestions_in_orderEN = {  { "Galadriel", "Galadriel.jpg" }  , { "Saruman the White", "Saruman_the_White.jpg" }  , { "Eomer", "Eomer.jpg" } 
-                    , { "Theoden", "Theoden.jpg" }  , { "Eowyn", "Eowyn.jpg" }  , { "Grima Wormtongue", "Grima_Wormtongue.jpg" }    , { "Shadowfax", "Shadowfax.jpg" }  
-                    , { "Treebeard", "Treebeard.jpg" }  , { "Shelob", "Shelob.jpg" }    , { "Faramir", "Faramir.jpg" }  , { "Denethor", "Denethor.jpg" }    , { "Balrog", "Balrog.jpg" }
-                    , { "Frodo Baggins", "Frodo_Baggins.jpg" } , { "Gandalf the Grey", "Gandalf_the_Grey.jpg" }    , { "Sam", "Sam.jpg" }  , { "Merry", "Merry.jpg" }  , { "Pippin", "Pippin.jpg" } 
-                    , { "Aragorn", "Aragorn.jpg" }  , { "Legolas", "Legolas.jpg" }  , { "Gimli", "Gimli.jpg" }  , { "Boromir", "Boromir.jpg" }  , { "Sauron", "Sauron.jpg" }  
-                    , { "The Nazgul", "The_Nazgul.jpg" }    , { "Gollum", "Gollum.jpg" }    , { "Bilbo Baggins", "Bilbo_Baggins.jpg" }  , { "Elrond", "Elrond.jpg" } 
-                    , { "Arwen Evenstar", "Arwen_Evenstar.jpg" }
+                string[,] allQuestions_in_orderEN = {
+  { "Aragorn", "https://drive.google.com/uc?id=1JGdq6EvIkQJkYG22fIBdQhAM5yNpAq30" }
+, { "Arkenstone", "https://drive.google.com/uc?id=1SgcOTypYKukJ6Rgp-8oBOlR_BMI1k2Am" }
+, { "Arwen Evenstar", "https://drive.google.com/uc?id=1RyMsEBK6Z35wqwyBfAmeDBo-kBwXE76Z" }
+, { "Athelas", "https://drive.google.com/uc?id=1uLimw7TX1CzE_vhmgTdpyyH03FjUTeHm" }
+, { "Azog", "https://drive.google.com/uc?id=1JeJj8OFQAdfknb1-FYPP-5zT-s8wbO_m" }
+, { "Balin", "https://drive.google.com/uc?id=18WjSkqw1rOuWMJ6gVch3daWqeKFHVjpr" }
+, { "Balrog", "https://drive.google.com/uc?id=11ZDyWRED_K9hh8lmWSS70uCl4cbRg8CY" }
+, { "Barahir", "https://drive.google.com/uc?id=10sXEfGvtBStsN6EK2fuebZbO30SX5zBr" }
+, { "Beorn", "https://drive.google.com/uc?id=1trzXHI2qkVzdorqZLLp63wErfHD4k7QP" }
+, { "Bilbo Baggins", "https://drive.google.com/uc?id=16U4n1LaHahmw3uruAkWByaaxLUlwB_hA" }
+, { "Bolg", "https://drive.google.com/uc?id=1FsYH-K9RWsa7p3XOJfbRgztOE_sbMfTk" }
+, { "Boromir", "https://drive.google.com/uc?id=1raEjDUFQlqZ8mVYyWe5ESKXbRsd0IfFf" }
+, { "Bowman Bard", "https://drive.google.com/uc?id=16_KCm9Rg6hNKEopPAaEMB2Wv60rdwots" }
+, { "Dain II Ironfoot", "https://drive.google.com/uc?id=1KTbHGZJomw1Pn1anQzY5Rg_fCzVCX-mZ" }
+, { "Denethor", "https://drive.google.com/uc?id=1jATABiQiRnepck8ilQnaunVV19oPii0q" }
+, { "Elrond", "https://drive.google.com/uc?id=19j00cdFwgAd2xcoTgWgmxWMvy9wV9ZPk" }
+, { "Eomer", "https://drive.google.com/uc?id=1-10IAgtxNaD3ZIXmFde5qpDLEiERInmB" }
+, { "Eowyn", "https://drive.google.com/uc?id=1ndMEcpl4wyofLMFfh_OO4UFnqVcIEmlu" }
+, { "Faramir", "https://drive.google.com/uc?id=1_99oI3i8hl4ioTlcCw7CVDpYdtd9v31I" }
+, { "Frodo Baggins", "https://drive.google.com/uc?id=1XRdns65YK9goZD_8xJ2LEgIJY6t0mFmx" }
+, { "Galadriel", "https://drive.google.com/uc?id=1BIAuUFa0KiI5bcKQTYdov18Q1Hn3neWc" }
+, { "Gandalf the Grey", "https://drive.google.com/uc?id=1J8OZDDqb_iLw5e4EfG9N6neSpc3F02z2" }
+, { "Gimli", "https://drive.google.com/uc?id=1jJjTLeArLOMv9gBT1xc4InpdRgGprZ8y" }
+, { "Gollum", "https://drive.google.com/uc?id=16Ayc46q687kR3o4LimdBHEwKI-d0BObX" }
+, { "Grima Wormtongue", "https://drive.google.com/uc?id=1ANBQJ6y83ZL5fBUsv0kDiMYtS3ybrQN-" }
+, { "Isildur", "https://drive.google.com/uc?id=1gVVB010zMD1hz3b5xNPkNqDwR5cSmbO3" }
+, { "Legolas", "https://drive.google.com/uc?id=1cwfc8ia0RlarsqPzV9HOIklARJy17mp3" }
+, { "Lembas bread", "https://drive.google.com/uc?id=1omGo6D__711lqZdU5ZjxItxwsK5p9Mq7" }
+, { "Lonely mountain", "https://drive.google.com/uc?id=1SOFIgb29qeL3ybFnPVfCg6L4IT0zDaRg" }
+, { "Merry", "https://drive.google.com/uc?id=1JBwePcmG31kLPkZisuDGAOU7ZXwY4PaP" }
+, { "Mithril", "https://drive.google.com/uc?id=1atnP5tH40LG7guXXJn5weggWpbXX7rOw" }
+, { "Morgul blades", "https://drive.google.com/uc?id=13bOwax3OygN41TS_q0FprcfO9txth5pI" }
+, { "Narya", "https://drive.google.com/uc?id=1uswR3O-96lDvyXAh1AvykHOVEiaCLzgA" }
+, { "Nenya", "https://drive.google.com/uc?id=1jy_Lp1zV-hut-wyHvSUL7ATpwxGq3PVw" }
+, { "Palantiri", "https://drive.google.com/uc?id=1VAbw7KXHnGuiV6q_Meg5k_ddhVVbkJaM" }
+, { "Phial of Galadriel", "https://drive.google.com/uc?id=1TXZiSehh7V0g4SHeInPK_fObdNp9jtvO" }
+, { "Pippin", "https://drive.google.com/uc?id=1LH0ljlbQS31nVWY3At2-xmwrfnahpg9u" }
+, { "Radagast", "https://drive.google.com/uc?id=14ni4O9_zVSbws8lEOCCZgsMdt2KvP7zV" }
+, { "Sam", "https://drive.google.com/uc?id=1XEwkXddgN60HgYz2Yqf4vXJKBYaoJ4fS" }
+, { "Saruman the White", "https://drive.google.com/uc?id=1jQ0_jq20xcbkg0NjCrTQWD1Ga-3FHBa4" }
+, { "Sauron", "https://drive.google.com/uc?id=1WEBOGbAIUe5XVSiNWlkIFGnJHSQ5xUD8" }
+, { "Shadowfax", "https://drive.google.com/uc?id=1UUMh6xfvEsRCBZjzwxA_kP4D9NJh-oWF" }
+, { "Shelob", "https://drive.google.com/uc?id=1fllCixlHp_fpi1tYRajP9QY2jEacXO9p" }
+, { "Smaug", "https://drive.google.com/uc?id=1oUL5Z7Pqh6UdFDiIPkK6DQfLRgeGzr4L" }
+, { "The Nazgul", "https://drive.google.com/uc?id=1dDBQntjnFdi-3K63DjgqElM-z6d5rTKH" }
+, { "Theoden", "https://drive.google.com/uc?id=1jOv4gATjZRSlQHkzrBVEW8sYpMkUbd9Z" }
+, { "Thorin Oakenshield", "https://drive.google.com/uc?id=1zn-dSjeSyGmO7ropGdK0CI7d5IqOtYz8" }
+, { "Tranduil", "https://drive.google.com/uc?id=17uYRN3SlLKxRZmiV6pJKOsBNzbZXVqdf" }
+, { "Treebeard", "https://drive.google.com/uc?id=1TXGa0xqL-7TtTCw48NX836fxglvsdh4V" }
+, { "Vilya", "https://drive.google.com/uc?id=1dwaxjpA2G7T414zdxMEnrBODVZnRwzTO" }
+, { "Witch-king of Angmar", "https://drive.google.com/uc?id=1HMG_sbDoCBjxJC4b7EW1QlPizRAyQ32R" }
 
 
                 };
@@ -29,8 +74,8 @@ namespace IsThis.Decks
 
 
                 Global.Deckcounter = countEN;
-                Global.DeckName = "Lord of the Rings Deck";
-                Global.DeckInfo = "deck info";
+                Global.DeckName = "Lord of the Rings & Hobbit Deck";
+                Global.DeckInfo = "Lord of the Rings & Hobbit characters and items";
                 Randomizing.Randomize(allQuestions_in_orderEN);
 
 
@@ -44,13 +89,58 @@ namespace IsThis.Decks
             {
                 string[,] allQuestions_in_orderLT = {
 
-                 { "Galandrielė", "Galadriel.jpg" }  , { "Sarumanas Baltasis", "Saruman_the_White.jpg" } , { "Eomeras", "Eomer.jpg" }    , { "Theodenas", "Theoden.jpg" }    
-                    , { "Eovyna", "Eowyn.jpg" } , { "Grima Sliužburnis", "Grima_Wormtongue.jpg" }   , { "Žvaigždikis", "Shadowfax.jpg" }    , { "Medžiabarzdis", "Treebeard.jpg" } 
-                    , { "Šeloba", "Shelob.jpg" }    , { "Faramiras", "Faramir.jpg" }    , { "Denetoras", "Denethor.jpg" }   , { "Balgoras", "Balrog.jpg" } , 
-                 { "Frodas Beginsas", "Frodo_Baggins.jpg" } , { "Gendalfas Pilkasis", "Gandalf_the_Grey.jpg" }  , { "Semas", "Sam.jpg" }    , { "Meris", "Merry.jpg" }
-                    , { "Pipinas", "Pippin.jpg" }   , { "Aragornas", "Aragorn.jpg" }    , { "Legolasas", "Legolas.jpg" }    , { "Gimlis", "Gimli.jpg" } , 
-                 { "Boromiras", "Boromir.jpg" }    , { "Sauronas", "Sauron.jpg" }  , { "Nazgulai", "The_Nazgul.jpg" }  , { "Golumas", "Gollum.jpg" }   ,
-                 { "Bilbas Beginsas", "Bilbo_Baggins.jpg" }    , { "Elroldas", "Elrond.jpg" }  , { "Arvena", "Arwen_Evenstar.jpg" }
+                
+  { "Aragornas", "https://drive.google.com/uc?id=1JGdq6EvIkQJkYG22fIBdQhAM5yNpAq30" }
+, { "Treino akmuo", "https://drive.google.com/uc?id=1SgcOTypYKukJ6Rgp-8oBOlR_BMI1k2Am" }
+, { "Arvena", "https://drive.google.com/uc?id=1RyMsEBK6Z35wqwyBfAmeDBo-kBwXE76Z" }
+, { "Atelasas", "https://drive.google.com/uc?id=1uLimw7TX1CzE_vhmgTdpyyH03FjUTeHm" }
+, { "Azogas", "https://drive.google.com/uc?id=1JeJj8OFQAdfknb1-FYPP-5zT-s8wbO_m" }
+, { "Balinas", "https://drive.google.com/uc?id=18WjSkqw1rOuWMJ6gVch3daWqeKFHVjpr" }
+, { "Balgoras", "https://drive.google.com/uc?id=11ZDyWRED_K9hh8lmWSS70uCl4cbRg8CY" }
+, { "Barahir", "https://drive.google.com/uc?id=10sXEfGvtBStsN6EK2fuebZbO30SX5zBr" }
+, { "Beornas", "https://drive.google.com/uc?id=1trzXHI2qkVzdorqZLLp63wErfHD4k7QP" }
+, { "Bilbas Beginsas", "https://drive.google.com/uc?id=16U4n1LaHahmw3uruAkWByaaxLUlwB_hA" }
+, { "Bolgas", "https://drive.google.com/uc?id=1FsYH-K9RWsa7p3XOJfbRgztOE_sbMfTk" }
+, { "Boromiras", "https://drive.google.com/uc?id=1raEjDUFQlqZ8mVYyWe5ESKXbRsd0IfFf" }
+, { "Lankininkas Bardas", "https://drive.google.com/uc?id=16_KCm9Rg6hNKEopPAaEMB2Wv60rdwots" }
+, { "Deinas II ", "https://drive.google.com/uc?id=1KTbHGZJomw1Pn1anQzY5Rg_fCzVCX-mZ" }
+, { "Denetoras", "https://drive.google.com/uc?id=1jATABiQiRnepck8ilQnaunVV19oPii0q" }
+, { "Elroldas", "https://drive.google.com/uc?id=19j00cdFwgAd2xcoTgWgmxWMvy9wV9ZPk" }
+, { "Eomeras", "https://drive.google.com/uc?id=1-10IAgtxNaD3ZIXmFde5qpDLEiERInmB" }
+, { "Eovyna", "https://drive.google.com/uc?id=1ndMEcpl4wyofLMFfh_OO4UFnqVcIEmlu" }
+, { "Faramiras", "https://drive.google.com/uc?id=1_99oI3i8hl4ioTlcCw7CVDpYdtd9v31I" }
+, { "Frodas Beginsas", "https://drive.google.com/uc?id=1XRdns65YK9goZD_8xJ2LEgIJY6t0mFmx" }
+, { "Galandrielė", "https://drive.google.com/uc?id=1BIAuUFa0KiI5bcKQTYdov18Q1Hn3neWc" }
+, { "Gendalfas Pilkasis", "https://drive.google.com/uc?id=1J8OZDDqb_iLw5e4EfG9N6neSpc3F02z2" }
+, { "Gimlis", "https://drive.google.com/uc?id=1jJjTLeArLOMv9gBT1xc4InpdRgGprZ8y" }
+, { "Golumas", "https://drive.google.com/uc?id=16Ayc46q687kR3o4LimdBHEwKI-d0BObX" }
+, { "Grima Sliužburnis", "https://drive.google.com/uc?id=1ANBQJ6y83ZL5fBUsv0kDiMYtS3ybrQN-" }
+, { "Isilduras", "https://drive.google.com/uc?id=1gVVB010zMD1hz3b5xNPkNqDwR5cSmbO3" }
+, { "Legolasas", "https://drive.google.com/uc?id=1cwfc8ia0RlarsqPzV9HOIklARJy17mp3" }
+, { "Lambaso duona", "https://drive.google.com/uc?id=1omGo6D__711lqZdU5ZjxItxwsK5p9Mq7" }
+, { "Vienišasis kalnas", "https://drive.google.com/uc?id=1SOFIgb29qeL3ybFnPVfCg6L4IT0zDaRg" }
+, { "Meris", "https://drive.google.com/uc?id=1JBwePcmG31kLPkZisuDGAOU7ZXwY4PaP" }
+, { "Mitrilas", "https://drive.google.com/uc?id=1atnP5tH40LG7guXXJn5weggWpbXX7rOw" }
+, { "Morgulo durklas", "https://drive.google.com/uc?id=13bOwax3OygN41TS_q0FprcfO9txth5pI" }
+, { "Naria", "https://drive.google.com/uc?id=1uswR3O-96lDvyXAh1AvykHOVEiaCLzgA" }
+, { "Nenia", "https://drive.google.com/uc?id=1jy_Lp1zV-hut-wyHvSUL7ATpwxGq3PVw" }
+, { "Palantiras", "https://drive.google.com/uc?id=1VAbw7KXHnGuiV6q_Meg5k_ddhVVbkJaM" }
+, { "Žvaigždės šviesa", "https://drive.google.com/uc?id=1TXZiSehh7V0g4SHeInPK_fObdNp9jtvO" }
+, { "Pipinas", "https://drive.google.com/uc?id=1LH0ljlbQS31nVWY3At2-xmwrfnahpg9u" }
+, { "Radagastas", "https://drive.google.com/uc?id=14ni4O9_zVSbws8lEOCCZgsMdt2KvP7zV" }
+, { "Semas", "https://drive.google.com/uc?id=1XEwkXddgN60HgYz2Yqf4vXJKBYaoJ4fS" }
+, { "Sarumanas Baltasis", "https://drive.google.com/uc?id=1jQ0_jq20xcbkg0NjCrTQWD1Ga-3FHBa4" }
+, { "Sauronas", "https://drive.google.com/uc?id=1WEBOGbAIUe5XVSiNWlkIFGnJHSQ5xUD8" }
+, { "Žvaigždikis", "https://drive.google.com/uc?id=1UUMh6xfvEsRCBZjzwxA_kP4D9NJh-oWF" }
+, { "Šeloba", "https://drive.google.com/uc?id=1fllCixlHp_fpi1tYRajP9QY2jEacXO9p" }
+, { "Smogas", "https://drive.google.com/uc?id=1oUL5Z7Pqh6UdFDiIPkK6DQfLRgeGzr4L" }
+, { "Nazgulai", "https://drive.google.com/uc?id=1dDBQntjnFdi-3K63DjgqElM-z6d5rTKH" }
+, { "Theodenas", "https://drive.google.com/uc?id=1jOv4gATjZRSlQHkzrBVEW8sYpMkUbd9Z" }
+, { "Torinas Ąžuolskydis", "https://drive.google.com/uc?id=1zn-dSjeSyGmO7ropGdK0CI7d5IqOtYz8" }
+, { "Tranduilas", "https://drive.google.com/uc?id=17uYRN3SlLKxRZmiV6pJKOsBNzbZXVqdf" }
+, { "Medžiabarzdis", "https://drive.google.com/uc?id=1TXGa0xqL-7TtTCw48NX836fxglvsdh4V" }
+, { "Vilia", "https://drive.google.com/uc?id=1dwaxjpA2G7T414zdxMEnrBODVZnRwzTO" }
+, { "Angmaro Karalius", "https://drive.google.com/uc?id=1HMG_sbDoCBjxJC4b7EW1QlPizRAyQ32R" }
 
                 };
 
@@ -59,7 +149,7 @@ namespace IsThis.Decks
 
 
                 Global.Deckcounter = countLT;
-                Global.DeckName = "Žiedų valdovo kaladė";
+                Global.DeckName = "Žiedų valdovo ir Hobito kaladė";
                 Global.DeckInfo = "Kaladės informacija";
                 Randomizing.Randomize(allQuestions_in_orderLT);
 
